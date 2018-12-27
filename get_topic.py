fff27179b1e18d6b7e90c4dc746bcba4a4750e7b
天逸 <albert.xht@alibaba-inc.com>
2018-12-27 17:54:41 +0800

    modify filter out topic

--- get_topic.py
+++ get_topic.py
@@ -1,25 +1,63 @@
 from collections import Counter
 import _pickle as pkl
 from topmine_scr import utils
+import tensorflow as tf
+from fuzzywuzzy import fuzz
+import re
+import _pickle as pkl
+import json
+
+flags = tf.flags
+
+FLAGS = flags.FLAGS
+
+## Required parameters
+flags.DEFINE_string(
+    "mining_path", None,
+    "Input TF example files (can be a glob or comma separated).")
+
+flags.DEFINE_string(
+    "prediction_path", None,
+    "Input TF example files (can be a glob or comma separated).")
+
+flags.DEFINE_string(
+    "doc_path", None,
+    "Input TF example files (can be a glob or comma separated).")
+
+flags.DEFINE_string(
+    "vocab_path", None,
+    "class-related topic and class-unrelated topic")
+
+flags.DEFINE_path(
+    "id2label_path", None,
+    "Input TF example files (can be a glob or comma separated).")
+
+flags.DEFINE_string(
+    "label_mapping_path", None,
+    "Input TF example files (can be a glob or comma separated).")
+
+flags.DEFINE_string(
+    "output_path", None,
+    "Input TF example files (can be a glob or comma separated).")
 
 def read_topic_model(path):
 	with open(path, "rb") as frobj:
-		topic_info = pkl.load(frobj)
+		mining_info = pkl.load(frobj)
 
-	return topic_info
+	return mining_info
 
-def get_frequent_phrases(topic_info):
-	return topic_info["frequent_phrases"]
+def get_frequent_phrases(mining_info):
+	return mining_info["frequent_phrases"]
 
-def get_topics(topic_info):
-	return topic_info["topics"]
+def get_topics(mining_info):
+	return mining_info["topics"]
 
-def get_index_word(topic_info):
-	return topic_info["index_vocab"]
+def get_index_word(mining_info):
+	return mining_info["index_vocab"]
 
-def get_most_frequent_topics(topic_info):
-	topics = get_topics(topic_info)
-	get_index_word = get_index_word(topic_info)
+def get_most_frequent_topics(mining_info):
+	topics = get_topics(mining_info)
+	index_vocab = get_index_word(mining_info)
 	output = []
     topic_index = 0
     for topic in topics:
@@ -34,5 +72,94 @@ def get_most_frequent_topics(topic_info):
         topic_index += 1
     return output
 
-def postprocess_frequent_phrase(frequent_phrases):
-	pass
+def parse_phrases(mining_info, prediction_info):
+    data = mining_info["frequent_phrases"]
+    pred = prediction_info["pred_label"]
+
+    indexer = mining_info["frequent_phrases"]["indexer"]["partitioned_docs_indexer"]
+
+    for p, d in zip(pred, data):
+        if p == 0:
+            continue
+        else:
+            filtered.append(d)
+    return filtered
+
+def get_label_mapping(label_mapping_path):
+    with open(label_mapping_path, "r") as frobj:
+        mapping_dict = json.load(frobj)
+        return mapping_dict
+
+def get_indicator(mining_info, prediction_info, doc_path, vocab_path,
+                id2label_path, label_mapping_path, output_path, **kargs):
+
+    label_mapping = get_label_mapping(label_mapping_path)
+
+    docs = utils.load_partitioned_docs(doc_path)
+    index_word = utils.load_vocab(vocab_path)
+
+    id2label = json.load(open(id2label_path, "r"))
+
+    str_lst = []
+    for i, doc in enumerate(docs):
+        string = []
+        for sub_word in doc:
+            string.append((" ".join([index_word[index] for index in sub_word])))
+        str_lst.append(string)
+
+    phrases = parse_phrases(mining_info, prediction_info)
+    indicator_lst = []
+
+    for s_index, s in enumerate(str_lst):
+        indicator = [0]*len(s)
+        pattern = []
+        for index, sub_s in enumerate(s):
+            if sub_s in phrases:
+                indicator[index] = 1
+                if len(pattern) >= 1:
+                    flag = 0
+                    for sub in pattern:
+                        score = fuzz.ratio(sub, sub_s)/ 100.0
+                        if score >= 0.9:
+                            flag = 1
+                    if flag == 0:
+                        pattern.append(sub_s)
+                else:
+                    pattern.append(sub_s)
+
+        pattern = [re.sub(" ", "", sub_pattern) for sub_pattern in pattern]
+
+        string_s = "".join(s.split())
+        string_id = indexer.get(string_s, "none")
+
+        label_id = indexer.get(string_id, "0")
+        cn_label = label_mapping[label_id]
+
+        indicator_lst.append({"indicator":indicator,
+                            "segmented_sentence":s,
+                             "pattern":"&".join(pattern),
+                             "doc2string_index":string_id,
+                             "label_id":label_id,
+                             "cn_label":cn_label
+                             })
+
+    pkl.dump(indicator_lst, open(output_path, "wb"))
+
+    return indicator_lst
+
+def main(_):
+
+    mining_info = pkl.load(open(FLAGS.mining_path))
+    prediction_info = pkl.load(open(FLAGS.prediction_path))
+
+    filtered_phrases = parse_phrases(mining_info, prediction_info)
+
+    indicator_lst = get_indicator(mining_info, prediction_info, FLAGS.doc_path, 
+                FLAGS.vocab_path, FLAGS.id2label_path, 
+                FLAGS.label_mapping_path, FLAGS.output_path)
+
+if __name__ == "__main__":
+    tf.app.run()
+
+
+	
---
+++ get_topic.sh
@@ -0,0 +1,8 @@
+python get_topic.py \
+	--mining_path "/data/xuht/websiteanalyze-data-seqing20180821/global_mining/mining_info.pkl" \
+	--prediction_path "/data/xuht/websiteanalyze-data-seqing20180821/global_mining/predict.pkl" \
+	--doc_path "/data/xuht/websiteanalyze-data-seqing20180821/global_mining/partioned_docs.txt" \
+	--vocab_path "/data/xuht/websiteanalyze-data-seqing20180821/global_mining/vocabs.txt" \
+	--id2label_path "/data/xuht/websiteanalyze-data-seqing20180821/global_mining/id2label.json" \
+	--label_mapping_path "/data/xuht/websiteanalyze-data-seqing20180821/global_mining/label_mapping.json" \
+	--output_path "/data/xuht/websiteanalyze-data-seqing20180821/global_mining/pattern.pkl" 
