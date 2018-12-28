1778516ef1acc28780dff4398411b95b6e417d9a
天逸 <albert.xht@alibaba-inc.com>
2018-12-28 14:06:06 +0800

    modify topic

--- get_topic.py
+++ get_topic.py
@@ -172,7 +172,7 @@ def main(_):
 	print("===filtered phrases===", len(filtered_phrases))
 
 	indicator_lst = get_indicator(mining_info, prediction_info, 
-				FLAGS.doc_path, FLAGS.vocab_path, 
+				FLAGS.doc_path, FLAGS.vocab_path, FLAGS.data_path,
 				FLAGS.label_mapping_path, FLAGS.output_path)
 
 if __name__ == "__main__":
