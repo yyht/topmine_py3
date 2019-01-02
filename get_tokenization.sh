468d31977d78e959e4b203c0c47c581bee89e8a6
天逸 <albert.xht@alibaba-inc.com>
2019-01-02 21:09:24 +0800

    finished

--- get_tokenization.sh
+++ get_tokenization.sh
@@ -2,7 +2,7 @@ python get_tokenization.py \
 	--raw_corpus "/data/xuht/websiteanalyze-data-seqing20180821/yancao.csv" \
 	--corpus "/data/xuht/product/yancao/corpus.txt" \
 	--model_prefix "/data/xuht/product/yancao/unigram" \
-	--vocab_size 5000 \
+	--vocab_size 32000 \
 	--model_type "unigram" \
 	--character_coverage 1.0 \
 	--tokenized_text "/data/xuht/product/yancao/unigram_tokenized.txt"
