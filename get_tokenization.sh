2e0e0ce0b5a7a46e6ebbeb60a414930fc95b6e2c
天逸 <albert.xht@alibaba-inc.com>
2019-01-02 17:53:02 +0800

    modify vocab size

--- get_tokenization.sh
+++ get_tokenization.sh
@@ -1,6 +1,6 @@
 python get_tokenization.py \
 	--corpus "/data/xuht/websiteanalyze-data-seqing20180821/yancao.txt" \
 	--model_prefix "/data/xuht/product/yancao/unigram" \
-	--vocab_size 50000 \
+	--vocab_size 10000 \
 	--model_type "unigram" \
 	--character_coverage 0.9995 \
