39cd36d9671588f5d0350eb4215fac66d3f8e21e
天逸 <albert.xht@alibaba-inc.com>
2019-01-02 17:40:12 +0800

    fix

--- get_tokenization.sh
+++ get_tokenization.sh
@@ -2,5 +2,5 @@ python get_tokenization.py \
 	--corpus "/data/xuht/websiteanalyze-data-seqing20180821/yancao.txt" \
 	--model_prefix "/data/xuht/product/yancao/sentence_piece_" \
 	--vocab_size 50000 \
-	--model_type "char" \
+	--model_type "unigram" \
 	--character_coverage 0.9995 \
