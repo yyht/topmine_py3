4f8deaccf4376e3f43c463fdd550b3f79071494c
天逸 <albert.xht@alibaba-inc.com>
2019-03-04 09:47:38 +0800

    add pkl free text script

---
+++ topic_mining_pkl.sh
@@ -0,0 +1,12 @@
+python topic_mining_pkl.py \
+	--train_file "/data/xuht/topmine/free_text/free_text.pkl" \
+	--stop_word_file "/data/xuht/stopwords-zh-master/stopwords-zh.txt" \
+	--ouput_file "/data/xuht/topmine/free_text/" \
+	--num_topics 2 \
+	--iteration 1000 \
+	--optimization_burnin 100 \
+	--alpha 4 \
+	--optimization_iterations 50 \
+	--beta 0.02 \
+	--min_support 10 \
+	--max_phrase_size 10
