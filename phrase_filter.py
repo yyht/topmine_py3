86f7bcdd05bcb6ae3a093f81c371f0f9f18360ad
天逸 <albert.xht@alibaba-inc.com>
2019-04-16 11:21:59 +0800

    add trie-tree word extractor

--- phrase_filter.py
+++ phrase_filter.py
@@ -2,6 +2,7 @@ from topmine_src import utils
 import re, json
 
 import numpy as np
+from flash_text import KeywordProcessor
 
 import tensorflow as tf
 
@@ -49,22 +50,21 @@ def main(_):
 	partioned_docs = result["partitioned_docs"]
 	doc_index = result["indexer"]
 
-	vocab2id, id2vocab = {}, {}
-	for index, word in enumerate(vocab_index):
-		vocab2id[word] = index
-		id2vocab[index] = word
+	keyword_detector = KeywordProcessor()
 
 	phrase_count = {}
 	for item in mined_phrases:
 		phrase_count[item[0]] = {}
 		phrase_count[item[0]]["count"] = item[1]
 		phrase_count[item[0]]["label"] = []
+		keyword_detector.add_keyword(item[0].split(), [item[0]])
 
-	for index, example in zip(doc_index, partioned_docs):
-		for phrase_id_lst in example:
-			phrase_string = " ".join([id2vocab[i] for i in phrase_id_lst])
-			if phrase_string in phrase_count:
-				phrase_count[phrase_string]["label"].append(examples[index]["label"])
+	for example in examples:
+		output = keyword_detector.extract_keywords(example["text"], span_info=True)
+    	word_lst = [item[0][0] for item in output]
+    	for word in word_lst:
+			if word in phrase_count:
+				phrase_count[phrase_string]["label"].append(example["label"])
 
 	with open(FLAGS.output_file, "wb") as fwobj:
 		pkl.dump(phrase_count, fwobj)
